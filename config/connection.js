f569c3b054f3bf98389c3d210bd0e42be828a3b3
Jahidul Hasan <hasanrabbie@gmail.com>
2026-02-25 06:31:43 +0600

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     port: process.env.DB_PORT,
-    host: "localhost",
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
